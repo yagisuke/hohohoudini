cc766e63a4bbb37b42c59d917179ee12ae647a85
yusuke-yagishita <yusuke.yagishita@dena.com>
2020-12-20 18:54:53 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/yagisuke"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Produce by yagisuke
         </a>
       </header>
     </div>
